789f0a5fa168bf237966a59a14b680c933220284
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2022-08-23 11:47:39 -0300

    performance updates and loggging

--- model.py
+++ model.py
@@ -17,7 +17,7 @@ RESIZED_IMAGE_HEIGHT = 150
 class Model:
 
     def __init__(self):
-        self.model = LinearSVC()
+        self.model = LinearSVC(dual=False)
 
         self.model_path = "model.sav"
 
@@ -28,13 +28,13 @@ class Model:
         self.test_size = 0.33
         self.seed = 13
 
-        self.cats_train_dir = "training_set/1"
+        self.cats_train_dir = "training_set/cats"
         self.cats_test_dir = "test_set/cats"
         self.cats_train_images = os.listdir(self.cats_train_dir)
         self.cats_test_images = os.listdir(self.cats_test_dir)
         self.cats_progress = None
 
-        self.dogs_train_dir = "training_set/2"
+        self.dogs_train_dir = "training_set/dogs"
         self.dogs_test_dir = "test_set/dogs"
         self.dogs_train_images = os.listdir(self.dogs_train_dir)
         self.dogs_test_images = os.listdir(self.dogs_test_dir)
@@ -45,8 +45,8 @@ class Model:
             self.model = pickle.load(open(self.model_path, 'rb'))
             logger.info(f"Model loaded from file {self.model_path}")
         else:
-            img_list = np.array([])
-            class_list = np.array([])
+            cats_img_list = np.array([])
+            dogs_img_list = np.array([])
 
             logger.info("Starting the training with cat images...")
 
@@ -60,8 +60,7 @@ class Model:
                     img = cv.imread(f)[:, :, 0]
                     img_resized = cv.resize(img, (RESIZED_IMAGE_WIDTH, RESIZED_IMAGE_HEIGHT))
                     img_reshaped = img_resized.reshape((1, RESIZED_IMAGE_WIDTH * RESIZED_IMAGE_HEIGHT))
-                    img_list = np.append(img_list, [img_reshaped])
-                    class_list = np.append(class_list, 1)
+                    cats_img_list = np.append(cats_img_list, [img_reshaped])
                     self.cats_progress.set_description(
                         "Training with cat images...".format(self.cats_train_images.index(filename)))
                     self.cats_progress.update(1)
@@ -79,23 +78,37 @@ class Model:
                     img = cv.imread(f)[:, :, 0]
                     img_resized = cv.resize(img, (RESIZED_IMAGE_WIDTH, RESIZED_IMAGE_HEIGHT))
                     img_reshaped = img_resized.reshape((1, RESIZED_IMAGE_WIDTH * RESIZED_IMAGE_HEIGHT))
-                    img_list = np.append(img_list, [img_reshaped])
-                    class_list = np.append(class_list, 2)
+                    dogs_img_list = np.append(dogs_img_list, [img_reshaped])
                     self.dogs_progress.set_description(
                         "Training with dog images...".format(self.dogs_train_images.index(filename)))
                     self.dogs_progress.update(1)
 
-            logger.info("Done the training with cat images...")
+            logger.info("Done the training with dog images...")
 
             self.cats_progress.close()
             self.dogs_progress.close()
 
-            img_list = img_list.reshape(cats_train_counter + dogs_train_counter,
+            logger.info("Finishing the training...")
+
+            cats_class_list = np.repeat(1, cats_train_counter)
+            dogs_class_list = np.repeat(2, dogs_train_counter)
+
+            logger.info("Doing the IA magic stuff...")
+
+            X = np.concatenate((cats_img_list, dogs_img_list), axis=None)
+            Y = np.concatenate((cats_class_list, dogs_class_list), axis=None)
+
+            X = X.reshape(cats_train_counter + dogs_train_counter,
                                         RESIZED_IMAGE_WIDTH * RESIZED_IMAGE_HEIGHT)
 
-            self.X_train, self.X_test, self.Y_train, self.Y_test = train_test_split(img_list, class_list,
+            logger.info("Learning more about cats and dogs...")
+
+            self.X_train, self.X_test, self.Y_train, self.Y_test = train_test_split(X, Y,
                                                                                     test_size=self.test_size,
                                                                                     random_state=self.seed)
+
+            logger.info("Fitting the model and saving it...")
+
             self.model.fit(self.X_train, self.Y_train)
 
             pickle.dump(self.model, open(self.model_path, 'wb'))
