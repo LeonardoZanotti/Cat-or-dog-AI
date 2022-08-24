98e315fc7b808b027fa3b9f2683848afa29a082b
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2022-08-24 10:38:47 -0300

    using multithreading to increse performance of the training

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import pickle
+import threading
 
 import cv2 as cv
 import numpy as np
@@ -12,8 +13,8 @@ logger = logging.getLogger('ftpuploader')
 logging.basicConfig(level=logging.INFO,
                     format='\n%(asctime)s :: %(levelname)s :: %(message)s')
 
-RESIZED_IMAGE_WIDTH = 500
-RESIZED_IMAGE_HEIGHT = 500
+RESIZED_IMAGE_WIDTH = 5
+RESIZED_IMAGE_HEIGHT = 5
 RESIZED_TUPLE = (RESIZED_IMAGE_WIDTH, RESIZED_IMAGE_HEIGHT)
 RESIZED_PRODUCT = RESIZED_IMAGE_WIDTH * RESIZED_IMAGE_HEIGHT
 NUM_TREES = 200
@@ -35,60 +36,64 @@ class Model:
 
         self.cats_dir = "cats"
         self.cats_images = os.listdir(self.cats_dir)
+        self.cats_img_list = np.array([])
         self.cats_progress = None
 
         self.dogs_dir = "dogs"
         self.dogs_images = os.listdir(self.dogs_dir)
+        self.dogs_img_list = np.array([])
         self.dogs_progress = None
+
+    def train_cats(self, cats_counter: int) -> None:
+        logger.info("Starting the training with cat images...")
+        self.cats_progress = tqdm(
+            total=cats_counter, position=0, leave=False)
+        for filename in self.cats_images:
+            f = os.path.join(self.cats_dir, filename)
+            if os.path.isfile(f):
+                img = cv.imread(f)[:, :, 0]
+                img_resized = cv.resize(img, RESIZED_TUPLE)
+                img_reshaped = img_resized.reshape((1, RESIZED_PRODUCT))
+                self.cats_img_list = np.append(self.cats_img_list, [img_reshaped])
+                self.cats_progress.set_description(
+                    "Training with cat images...".format(self.cats_images.index(filename)))
+                self.cats_progress.update(1)
+        self.cats_progress.close()
+        logger.info("Done the training with cat images...")
+
+    def train_dogs(self, dogs_counter: int) -> None:
+        logger.info("Starting the training with dog images...")
+        self.dogs_progress = tqdm(
+            total=dogs_counter, position=0, leave=False)
+        for filename in self.dogs_images:
+            f = os.path.join(self.dogs_dir, filename)
+            if os.path.isfile(f):
+                img = cv.imread(f)[:, :, 0]
+                img_resized = cv.resize(img, RESIZED_TUPLE)
+                img_reshaped = img_resized.reshape((1, RESIZED_PRODUCT))
+                self.dogs_img_list = np.append(self.dogs_img_list, [img_reshaped])
+                self.dogs_progress.set_description(
+                    "Training with dog images...".format(self.dogs_images.index(filename)))
+                self.dogs_progress.update(1)
+        self.dogs_progress.close()
+        logger.info("Done the training with dog images...")
 
     def train_model(self) -> None:
         if os.path.isfile(self.model_path):
             self.model = pickle.load(open(self.model_path, 'rb'))
             logger.info(f"Model loaded from file {self.model_path}")
         else:
-            cats_img_list = np.array([])
-            dogs_img_list = np.array([])
-
-            logger.info("Starting the training with cat images...")
-
-            # train cats
             cats_counter = len([file for file in os.listdir(self.cats_dir) if
                                 os.path.isfile(os.path.join(self.cats_dir, file))])
-            self.cats_progress = tqdm(
-                total=cats_counter, position=0, leave=False)
-            for filename in self.cats_images:
-                f = os.path.join(self.cats_dir, filename)
-                if os.path.isfile(f):
-                    img = cv.imread(f)[:, :, 0]
-                    img_resized = cv.resize(img, RESIZED_TUPLE)
-                    img_reshaped = img_resized.reshape((1, RESIZED_PRODUCT))
-                    cats_img_list = np.append(cats_img_list, [img_reshaped])
-                    self.cats_progress.set_description(
-                        "Training with cat images...".format(self.cats_images.index(filename)))
-                    self.cats_progress.update(1)
-            self.cats_progress.close()
-
-            logger.info("Done the training with cat images...")
-            logger.info("Starting the training with dog images...")
-
-            # train dogs
             dogs_counter = len([file for file in os.listdir(self.dogs_dir) if
                                 os.path.isfile(os.path.join(self.dogs_dir, file))])
-            self.dogs_progress = tqdm(
-                total=dogs_counter, position=0, leave=False)
-            for filename in self.dogs_images:
-                f = os.path.join(self.dogs_dir, filename)
-                if os.path.isfile(f):
-                    img = cv.imread(f)[:, :, 0]
-                    img_resized = cv.resize(img, RESIZED_TUPLE)
-                    img_reshaped = img_resized.reshape((1, RESIZED_PRODUCT))
-                    dogs_img_list = np.append(dogs_img_list, [img_reshaped])
-                    self.dogs_progress.set_description(
-                        "Training with dog images...".format(self.dogs_images.index(filename)))
-                    self.dogs_progress.update(1)
-            self.dogs_progress.close()
 
-            logger.info("Done the training with dog images...")
+            threads = list()
+            threads.append(threading.Thread(target=self.train_cats, args=(cats_counter,), daemon=True))
+            threads.append(threading.Thread(target=self.train_dogs, args=(dogs_counter,), daemon=True))
+            for thread in threads: thread.start()
+            for thread in threads: thread.join()
+
             logger.info("Finishing the training...")
 
             cats_class_list = np.repeat(1, cats_counter)
@@ -96,7 +101,9 @@ class Model:
 
             logger.info("Learning more about cats and dogs...")
 
-            X = np.concatenate((cats_img_list, dogs_img_list), axis=None)
+            logger.info(f"{len(self.cats_img_list)} {len(self.dogs_img_list)}")
+
+            X = np.concatenate((self.cats_img_list, self.dogs_img_list), axis=None)
             Y = np.concatenate((cats_class_list, dogs_class_list), axis=None)
 
             X = X.reshape(cats_counter + dogs_counter, RESIZED_PRODUCT)
